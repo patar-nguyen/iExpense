5cdd3b5ac39e8a0689a5eb610658fdfaac8e5d3b
Patrick Nguyen <patar.nguyen@gmail.com>
2023-08-28 18:01:56 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Patrick Nguyen on 2023-08-28.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
